bef55e3ab62f6a8387d5d95b5a7c900aba1590d2
Chris DeMars <chrisdemars@rocketmortgage.com>
2021-10-12 13:59:38 -0400

    adds accordion component and styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
+import Accordions from './components/Accordions';
 import Buttons from './components/Buttons';
 
 function App() {
-    return (
-        <div className='App'>
-            <header className='App-header'>
-                <h1>Lightning Design System Demo!</h1>
-            </header>
-            <Buttons />
-        </div>
-    );
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Lightning Design System Demo!</h1>
+      </header>
+      <main className='grid-wrapper'>
+        <Buttons />
+        <Accordions />
+      </main>
+    </div>
+  );
 }
 
 export default App;
